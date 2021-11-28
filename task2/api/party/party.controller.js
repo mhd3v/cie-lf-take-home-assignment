a8b3e100b4e1ec6f12ead2290d7e9541297c658e
Mahad <mamir3012@gmail.com>
2021-11-28 20:44:06 +0500

    Add logic to get lng and lat against passed location

--- task2/api/party/party.controller.js
+++ task2/api/party/party.controller.js
@@ -14,6 +14,25 @@ const getPartyPlan = async (req, res) => {
       .filter((location) => location)
       .map((location) => location.trim());
 
+    const geocodeRequests = locations.map((location) => {
+      var encodedAddress = encodeURIComponent(location);
+      return axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.API_KEY}`
+      );
+    });
+
+    const responses = await Promise.all(geocodeRequests);
+    const predictionsRequests = responses.map((response) => {
+      // When location not found
+      if (response.data.status === "ZERO_RESULTS") {
+        return null;
+      }
+      let lat = response.data.results[0].geometry.location.lat;
+      let lng = response.data.results[0].geometry.location.lng;
+      return axios.get(
+        `https://api.brightsky.dev/weather?date=${from.toISOString()}&last_date=${to.toISOString()}&lat=${lat}&lon=${lng}`
+      );
+    });
     res.json({
       msg: "Dummy party plan!",
     });
--- task2/middleware/check-api-key.js
+++ task2/middleware/check-api-key.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res, next) {
   if (!process.env.API_KEY) {
     console.error(
-      '[error]: The "API_KEY" environment variable is required. Please set it the environment before proceeding.'
+      '[error]: The "API_KEY" environment variable is required. Please set it in the environment before proceeding.'
     );
     process.exit(1);
   }
